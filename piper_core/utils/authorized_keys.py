57c01478bf71844390f0516c8f72076a10d118b5
Martin Franc <me@martinfranc.eu>
2018-01-03 11:00:00 +0100

    Make path to Piper config in SSH absolute

--- piper_core/utils/authorized_keys.py
+++ piper_core/utils/authorized_keys.py
@@ -9,6 +9,7 @@ COMMAND = 'command="piper-shell {} {}",no-port-forwarding,no-X11-forwarding'
 
 
 def write(path: Path, config: Path, users: Iterable[User]) -> None:
+    config = config.absolute()
     with path.open('r') as fp:
         preserve = [line for line in fp if not line.endswith(KEY_COMMENT)]
     with path.open('w') as fp:
