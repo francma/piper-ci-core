a88ff4c59d849b0b1567f84d419067a464e76af3
Martin Franc <me@martinfranc.eu>
2018-01-03 13:08:11 +0100

    Allow whitespaces after PIPER_GENERATED commend in authorized_keys

--- piper_core/utils/authorized_keys.py
+++ piper_core/utils/authorized_keys.py
@@ -11,7 +11,7 @@ COMMAND = 'command="piper-shell {} {}",no-port-forwarding,no-X11-forwarding'
 def write(path: Path, config: Path, users: Iterable[User]) -> None:
     config = config.absolute()
     with path.open('r') as fp:
-        preserve = [line for line in fp if not line.endswith(KEY_COMMENT)]
+        preserve = [line for line in fp if not line.strip().endswith(KEY_COMMENT)]
     with path.open('w') as fp:
         for line in preserve:
             fp.write(line)
