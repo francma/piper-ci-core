8351dae5ce7bc222b05ca00823c6fd3e3c80f8be
Martin Franc <me@martinfranc.eu>
2018-02-11 00:56:53 +0100

    setup: specify dep versions

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     package_dir={'piper_core': 'piper_core'},
     author='Martin Franc',
     author_email='francma6@fit.cvut.cz',
-    keywords='lxd,ci,runner',
+    keywords='ci,github',
     license='Public Domain',
-    url='https://github.com/francma/piper-ci-lxd-runner',
+    url='https://github.com/francma/piper-ci-core',
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
@@ -29,11 +29,10 @@ setup(
     install_requires=[
         'peewee>=3',
         'Flask>=0.12',
-        'pyyaml',
-        'redis',
-        'simpleeval',
-        'requests',
-        'jsonschema',
+        'pyyaml>=3',
+        'redis>=2',
+        'simpleeval>=0.9',
+        'jsonschema>=2.6',
     ],
     extras_require={
         'dev': [
